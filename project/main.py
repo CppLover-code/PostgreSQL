270efc3ea0e86e144af5d9e32d91c99b5bbf974b
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-13 10:10:52 +0400

    !

--- project/main.py
+++ project/main.py
@@ -21,6 +21,8 @@ class User(Base):
     age = Column(Integer)
     city = Column(String)
 
+"""
+
 # Создание таблицы
 Base.metadata.create_all(engine)
 
@@ -38,12 +40,15 @@ with Session(engine) as session:
         User(name="Sara", age=27, city="Rome"),
         User(name="Tom", age=40, city="Madrid"),
         User(name="Lisa", age=24, city="Warsaw"),
-        User(name="David", age=31, city="Prague")
+        User(name="David", age=31, city="Prague"),
+        User(name="Anna", age=32, city="Batumi")
     ]
 
     session.add_all(users)                          # добавляет сразу список объектов.
     session.commit()
     print("Users added")
+
+"""
 
 # ***********************************************************************************
 # Получить ВСЕХ пользователей
@@ -70,4 +75,45 @@ with Session(engine) as session:
     print(user.id, user.name, user.age, user.city)
 
 # ***********************************************************************************
-# Получить ПЕРВОГО пользователя
+# Поиск по условию - filter_by()
+
+with Session(engine) as session:
+
+    user = session.query(User).filter_by(name='Anna').first() # поиск первого пользователя с именем Anna
+
+    print(user.id, user.name, user.age, user.city)
+
+# ***********************************************************************************
+# Поиск по условию - filter_by() нескольких пользователей
+
+with Session(engine) as session:
+
+    users = session.query(User).filter_by(city='Batumi').all()
+    print("******************************************")
+    for user in users:
+         print(user.id, user.name, user.age, user.city)
+
+"""
+SELECT * FROM users
+WHERE city = 'Batumi';
+"""
+
+# ***********************************************************************************
+# удаление всех пользователей, кроме первых 11
+
+with Session(engine) as session:
+
+    users_to_delete = (
+        session.query(User)
+        .order_by(User.id)          # сортируем по ID
+        .offset(11)                 # пропускаем первые 11 пользователей
+        .all()                      # получаем всех остальных
+    )
+
+    for user in users_to_delete:
+        session.delete(user)        # удаляем
+
+    session.commit()                # сохраняем изменения
+
+# ***********************************************************************************
+# filter() — более мощный поиск
