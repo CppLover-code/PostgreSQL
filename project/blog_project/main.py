d2a76fc9a5a88822f59097e98beaa593bae22143
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-05-14 11:21:10 +0400

    !

--- project/blog_project/main.py
+++ project/blog_project/main.py
@@ -8,3 +8,20 @@ AND / OR
 relationships между таблицами
 """
 
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from sqlalchemy.orm import DeclarativeBase, Session, relationship
+
+# Подключение
+engine = create_engine(
+    "postgresql://postgres:postgre212223@localhost:5432/blog_project"
+)
+
+# Base
+class Base(DeclarativeBase):
+    pass
+
+# =========================
+# USERS
+# =========================
